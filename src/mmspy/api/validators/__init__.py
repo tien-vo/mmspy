74a6f77dd01769c1492571153c73c615391aafb6
tvo <tvo.email@proton.me>
2024-10-14 14:31:32 -0600

    Api (query): Add validators for edp and fgm data types

--- src/mmspy/api/validators/__init__.py
+++ src/mmspy/api/validators/__init__.py
@@ -3,13 +3,16 @@ r"""Provide validators for query parameters."""
 __all__ = [
     "one_of",
     "convert_data_rate",
-    "reset_data_type",
-    "convert_fpi_data_type",
+    "convert_edp_data_type",
+    "convert_fgm_data_type",
     "convert_feeps_data_type",
+    "convert_fpi_data_type",
+    "reset_data_type",
 ]
 
+from ._edp import convert_edp_data_type
 from ._feeps import convert_feeps_data_type
-from ._fgm import reset_data_type
+from ._fgm import convert_fgm_data_type, reset_data_type
 from ._fpi import convert_fpi_data_type
 from ._general import convert_data_rate, one_of
 from ._time import time_in_range, time_range_is_valid
