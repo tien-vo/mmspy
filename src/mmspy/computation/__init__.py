6193af093b83cd10f36293f4c155ff80fdd0b23c
tvo <tvo.email@proton.me>
2024-10-15 10:09:17 -0600

    Computation: Add docstring and expose api

--- src/mmspy/computation/__init__.py
+++ src/mmspy/computation/__init__.py
@@ -1,1 +1,32 @@
-r""".. todo:: Write docstring."""
+r"""Provide computational utilities."""
+
+__all__ = [
+    "curlometer",
+    "integrate_3d_distribution",
+    "interpolate_2d_distribution",
+    "interpolate_3d_distribution",
+    "smooth_distribution",
+    "cartesian_to_fac",
+    "fac_to_cartesian",
+    "quaternion_rotate",
+    "rotation_matrix",
+    "matrix_multiply",
+    "quaternion_conjugate",
+    "quaternion_dot",
+    "vector_norm",
+]
+
+from .curlometer import curlometer
+from .particles import (
+    integrate_3d_distribution,
+    interpolate_2d_distribution,
+    interpolate_3d_distribution,
+    smooth_distribution,
+)
+from .transform import (
+    cartesian_to_fac,
+    fac_to_cartesian,
+    quaternion_rotate,
+    rotation_matrix,
+)
+from .vector import matrix_multiply, quaternion_conjugate, quaternion_dot, vector_norm
