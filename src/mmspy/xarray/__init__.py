e8228b9feea0d0b177898e566689379a7f214fca
tvo <tvo.email@proton.me>
2024-10-15 10:03:56 -0600

    Xarray: Expose API

--- src/mmspy/xarray/__init__.py
+++ src/mmspy/xarray/__init__.py
@@ -1,3 +1,14 @@
+r"""Provide `xarray` accessors to datasets."""
+
+__all__ = [
+    "FeepsAccessor",
+    "FpiAccessor",
+    "SpeciesAccessor",
+    "RankOneAccessor",
+    "RankTwoAccessor",
+    "UnitsAccessor",
+]
+
 from .feeps.accessor import FeepsAccessor
 from .fpi import FpiAccessor
 from .species import SpeciesAccessor
