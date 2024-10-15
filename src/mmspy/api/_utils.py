8fa69bf84f00322de1967e65f8f4801a0c8a057c
tvo <tvo.email@proton.me>
2024-10-14 23:21:18 -0600

    Api (utils): Change bar format

--- src/mmspy/api/_utils.py
+++ src/mmspy/api/_utils.py
@@ -8,8 +8,8 @@ def bar_config(**kwargs) -> dict:
     r""".. todo:: Add docstring."""
     return {
         "bar_format": (
-            "[{bar:20}] | {desc:70} "
-            "[{n_fmt}/{total_fmt} | {percentage:.0f}% | {rate_fmt}]"
+            "[{bar:16}] [{n_fmt}/{total_fmt} | {rate_fmt}]: "
+            "{desc:50}"
         ),
         "dynamic_ncols": True,
         "leave": True,
