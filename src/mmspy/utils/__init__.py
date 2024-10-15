f28a7f58910b03db5c4fc95dfa03e35f25f92cee
tvo <tvo.email@proton.me>
2024-10-15 15:23:41 -0600

    Utils: Expose api

--- src/mmspy/utils/__init__.py
+++ src/mmspy/utils/__init__.py
@@ -1,1 +1,12 @@
 r"""Provide generic utility functions."""
+
+__all__ = [
+    "first_valid_index",
+    "last_valid_index",
+    "force_odd",
+    "match_time_resolution",
+    "sampling_information",
+]
+
+from .index import first_valid_index, last_valid_index
+from .timing import force_odd, match_time_resolution, sampling_information
