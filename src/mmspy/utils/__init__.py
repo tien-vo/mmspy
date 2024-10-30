6bce1aab69f6711b22b8e60dbcf97aaa30c97197
tvo <tvo.email@proton.me>
2024-10-30 04:38:15 -0600

    Utils (units): Add dataclass for units

--- src/mmspy/utils/__init__.py
+++ src/mmspy/utils/__init__.py
@@ -6,7 +6,9 @@ __all__ = [
     "force_odd",
     "match_time_resolution",
     "sampling_information",
+    "Units",
 ]
 
 from .index import first_valid_index, last_valid_index
 from .timing import force_odd, match_time_resolution, sampling_information
+from .units import Units
