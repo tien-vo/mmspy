09fcf061486526324f2eff80576c89575c87c7dd
tvo <tvo.email@proton.me>
2024-10-31 02:07:12 -0600

    Xarray (units): Disable in favor of pint

--- src/mmspy/xarray/__init__.py
+++ src/mmspy/xarray/__init__.py
@@ -6,11 +6,9 @@ __all__ = [
     "SpeciesAccessor",
     "RankOneAccessor",
     "RankTwoAccessor",
-    "UnitsAccessor",
 ]
 
 from .feeps.accessor import FeepsAccessor
 from .fpi import FpiAccessor
 from .species import SpeciesAccessor
 from .tensors import RankOneAccessor, RankTwoAccessor
-from .units import UnitsAccessor
