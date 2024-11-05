eef43dd1007a7124206ef5150e9e253711f218d5
tvo <tvo.email@proton.me>
2024-11-05 10:52:49 -0700

    Utils (units): Add docstring

--- src/mmspy/utils/units.py
+++ src/mmspy/utils/units.py
@@ -7,4 +7,17 @@ import xarray as xr
 
 
 def is_quantified(data: xr.DataArray) -> bool:
+    r"""Check if a data array is quantified with ``pint``.
+
+    Parameters
+    ----------
+    data : DataArray
+        Data array to check.
+
+    Returns
+    -------
+    is_quantified : bool
+        Condition
+
+    """
     return isinstance(data.data, pint.Quantity)
