abee3381d0237c4fe94b222e4b22eab38475c638
tvo <tvo.email@proton.me>
2024-11-05 12:46:10 -0700

    Mmspy (pint): Rename to units

--- src/mmspy/pint.py
+++
@@ -1,9 +0,0 @@
-r"""Unit support with `pint`."""
-
-__all__ = ["unit_registry"]
-
-import cf_xarray.units  # noqa: F401
-import pint_xarray  # noqa: F401
-from pint import application_registry as unit_registry
-
-unit_registry.formatter.default_format = "cf"
