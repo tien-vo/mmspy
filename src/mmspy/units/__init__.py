22d4df6b95a846c94c97ea0ed4cdf819deea82ec
tvo <tvo.email@proton.me>
2024-11-13 03:04:43 -0700

    Mmspy (units): Add D2 particle units table

--- src/mmspy/units/__init__.py
+++ src/mmspy/units/__init__.py
@@ -1,10 +1,12 @@
 r"""Provide unit support with `pint`."""
 
+from importlib.resources import files
+
 import pint
 import pint_xarray
 
-from mmspy.units._utils import _attach_exponential_symbol, _get_cache_path
 from mmspy.units._format import fits_formatter, latex_formatter
+from mmspy.units._utils import _attach_exponential_symbol, _get_cache_path
 
 registry = pint.UnitRegistry(
     force_ndarray_like=True,
@@ -14,4 +16,5 @@ registry = pint.UnitRegistry(
 )
 registry.formatter.default_format = "fits"
 registry.setup_matplotlib()
+registry.load_definitions(str(files("mmspy.units") / "data" / "particle.txt"))
 pint.set_application_registry(registry)
