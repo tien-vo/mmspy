7b163f01e670b72ced3b7867be273343d4b844c8
tien-vo <tvo.email@proton.me>
2025-06-18 22:46:33 -0600

    xarray, utils, computation: Migrate old modules

--- src/mmspy/computation/particle/integrate/__init__.py
+++
@@ -1,5 +0,0 @@
-from mmspy.computation.particle.integrate._reduce import reduce_distribution
-
-
-def integrate_distribution():
-    pass
--- src/mmspy/xarray/feeps/__init__.py
+++
@@ -1,5 +0,0 @@
-r"""Provide xarray accessor for FEEPS datasets."""
-
-__all__ = ["FeepsAccessor"]
-
-from .accessor import FeepsAccessor
