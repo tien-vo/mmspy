51dd665596196a07e29bafa11b722ee8e131dbf5
tien-vo <tvo.email@proton.me>
2025-06-22 15:00:24 -0500

    Tests: Merge test_utils

--- tests/utils/test_timing.py
+++ tests/utils/test_timing.py
@@ -1,19 +1,20 @@
-r"""Tests for functionality in `mmspy.utils.timing`.
+"""Tests for functionality in `mmspy.compute.timing`.
 
 .. todo::
     Add more tests
 """
 
+
 import numpy as np
 import pytest
 import xarray as xr
-from pint_xarray import unit_registry as u
 
-from mmspy.utils.timing import match_time_resolution
+from mmspy import units as u
+from mmspy.compute.utils import match_time_resolution
 
 
 class TestMatchTimeResolution:
-    r"""Tests for `match_time_resolution`."""
+    """Tests for `match_time_resolution`."""
 
     time = np.arange("2024-01-01T00", "2024-01-02T01", dtype="datetime64[h]")
     data = xr.DataArray(
@@ -25,6 +26,6 @@ class TestMatchTimeResolution:
     @pytest.mark.parametrize("step", range(1, 25, 1))
     @pytest.mark.parametrize("average", [True, False])
     def test_quantity_input(self, step, average):
-        r"""Test for target input as an astropy quantity."""
+        """Test for target input as an astropy quantity."""
         data = match_time_resolution(self.data, u.Quantity(step, "h"), average)
         assert (data == self.data[slice(0, -1, step)]).all()
