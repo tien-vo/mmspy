25266909e014cdd684acb44b4cbff7bad2ebb2e6
tien-vo <tvo.email@proton.me>
2025-10-30 10:10:53 -0600

    Configure (xarray): Add scm mask method

--- src/mmspy/configure/xarray/__init__.py
+++ src/mmspy/configure/xarray/__init__.py
@@ -5,8 +5,9 @@ import xarray as xr
 xr.set_options(keep_attrs=True)
 
 from mmspy.configure.xarray.edp import EdpAccessor
+from mmspy.configure.xarray.feeps import FeepsAccessor
 from mmspy.configure.xarray.fgm import FgmAccessor
 from mmspy.configure.xarray.fpi import FpiAccessor
-from mmspy.configure.xarray.feeps import FeepsAccessor
+from mmspy.configure.xarray.scm import ScmAccessor
 from mmspy.configure.xarray.species import SpeciesAccessor
 from mmspy.configure.xarray.tensors import TensorAccessor
